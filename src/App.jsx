2d6c4ebb1574d23d6eedfbfaecf8135b6eace5a4
jhslive <111948596+jhslive@users.noreply.github.com>
2022-10-07 10:47:29 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -169,7 +169,7 @@ function App() {
           <div
             style={{
               position: "absolute",
-              right: numLocLR + "px",
+              right: numLocLR + 300 + "px",
               top: numsLoc[0] + "px",
               fontSize: 18 + "px",
               color: "RGB(126,126,126)",
