8feb915584c99d21f1b6f9064e7881ac821d5c3e
jhslive <111948596+jhslive@users.noreply.github.com>
2022-10-13 12:56:13 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   const [numsLoc, setNumsLoc] = useState([227, 570, 570]);
   const [numLocLR, setNumLocLR] = useState(1000);
   const [leverage, setLeverage] = useState(75);
-  const [date, setdate] = useState(new Date().toLocaleString('en-US', { hour12: false,}).setInterval(date, 1000));
+  const [date, setdate] = useState(new Date().toLocaleString('en-US', { hour12: false,}));
   const [result, setResult] = useState(
     ((closingPrice / entryPrice - 1) * 75 * 100).toFixed(2)
   );
