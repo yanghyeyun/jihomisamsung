9f3a596cde2df20737ddcf1aae1f215688400064
jhslive <111948596+jhslive@users.noreply.github.com>
2022-11-02 11:11:49 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ import html2canvas from "html2canvas";
 
 function App() {
   const [coin, setCoin] = useState("누사이퍼");
-  const [name, setname] = useState("NU");
+  const [coinname, setcoinname] = useState("NU");
   const [askPrice, setaskPrice] = useState("500");
   const [bidPrice, setbidPrice] = useState("550");
   const [maxPrice, setmaxPrice] = useState("100000");
-  const [coincount, setcoincount] = useState(((maxPrice/askPrice) - (maxPrice/askPrice*0.0005)).toFixed(6));
+  const [Amount, setAmount] = useState(((maxPrice/askPrice) - (maxPrice/askPrice*0.0005)).toFixed(6));
   const [isLong, setIsLong] = useState(true);
   const [numsLoc, setNumsLoc] = useState([227, 570, 570]);
   const [numLocLR, setNumLocLR] = useState(1000);
@@ -228,7 +228,7 @@ function App() {
 
             }}
           >
-            {coincount}
+            {Amount}
           </div>
            <div
             style={{
