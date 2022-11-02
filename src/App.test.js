116218c320c1cc9144e6dfde8b4dcb291203c995
jhslive <111948596+jhslive@users.noreply.github.com>
2022-11-02 21:18:23 +0900

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -6,8 +6,3 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-module.exports = {
-  devServer: {
-    overlay: false
-  }
-}
