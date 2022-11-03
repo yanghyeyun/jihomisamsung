d436a039f006b133e4a8ed911bf3198097b87e83
jhslive <111948596+jhslive@users.noreply.github.com>
2022-11-03 09:58:55 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -171,7 +171,7 @@ function App() {
 
             }}
           >
-            {((maxPrice/askPrice - (maxPrice/askPrice*0.0005)).toFixed(6).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} 
+            {(maxPrice/askPrice - (maxPrice/askPrice*0.0005)).toFixed(6).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','} 
            </div>          
                     <div
             style={{
