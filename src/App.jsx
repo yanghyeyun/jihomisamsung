22b59f638890f8c01ecaa8041a008ca29ab9805d
jhslive <111948596+jhslive@users.noreply.github.com>
2022-11-03 11:04:30 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -229,7 +229,7 @@ function App() {
 
             }}
           >
-           {(Math.ceil((((maxPrice/askPrice)-(maxPrice/askPrice)*0.0005)*bidPrice) -(maxPrice/askPrice)*bidPrice*0.0005).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','))}
+           {((((maxPrice/askPrice)-(maxPrice/askPrice)*0.0005)*bidPrice) -(maxPrice/askPrice)*bidPrice*0.0005).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
           </div>
           <div
             style={{
@@ -257,7 +257,7 @@ function App() {
 
             }}
           >
-            {(Math.ceil(((((maxPrice/askPrice)-(maxPrice/askPrice)*0.0005)*bidPrice) -(maxPrice/askPrice)*bidPrice*0.0005)-maxPrice).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','))}
+            {(((((maxPrice/askPrice)-(maxPrice/askPrice)*0.0005)*bidPrice) -(maxPrice/askPrice)*bidPrice*0.0005)-maxPrice).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
           </div>
            <div
             style={{
