afb03346d2f02097a0fd6499247734dd097beadd
jhslive <111948596+jhslive@users.noreply.github.com>
2022-11-24 17:29:37 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ import tanguPic from "./tangu.png";
 import html2canvas from "html2canvas";
 
 function App() {
-  const [coin, setCoin] = useState("피르마체인");
-  const [price, setprice] = useState("FCT2");
+  const [coin, setCoin] = useState("에스티피");
+  const [price, setprice] = useState("STPT");
   const [won, setwon] = useState("KRW");
-  const [askPrice, setaskPrice] = useState("55"); 
-  const [bidPrice, setbidPrice] = useState("65");
+  const [askPrice, setaskPrice] = useState("49"); 
+  const [bidPrice, setbidPrice] = useState("63");
   const [maxPrice, setmaxPrice] = useState("100000");
   const [isLong, setIsLong] = useState(true); //eslint-disable-line no-unused-vars
   const [numsLoc, setNumsLoc] = useState([0, 0, 0]);
