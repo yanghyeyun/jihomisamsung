0a7a6b00e41bdeac4444b4a420b51cdd54bd62f7
jhslive <111948596+jhslive@users.noreply.github.com>
2023-02-08 17:44:19 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -122,7 +122,7 @@ function App() {
             style={{
               position: "absolute",
               right: "318px",
-              top: "186px",
+              top: "194px",
               fontSize: "40px",
               color: "rgb(241,10,68)",
               fontFamily: "NanumBarunGothic",
