66eb0ad814e25380a8980e385f81c2e2118c4904
jhslive <111948596+jhslive@users.noreply.github.com>
2023-02-09 10:49:09 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,12 @@ function App() {
   const [maxPrice, setmaxPrice] = useState("100000");
   const [isLong, setIsLong] = useState(true); //eslint-disable-line no-unused-vars
   const [leverage, setLeverage] = useState(1); //eslint-disable-line no-unused-vars
-  const [date, setdate] = useState(new Date());
+  const [date, setdate] = useState(var date = new Date();
+
+var month = today.getUTCMonth() + 1; //months from 1-12
+var day = today.getUTCDate();
+var year = today.getUTCFullYear();
+  );
   const [result, setResult] = useState(
     ((bidPrice / askPrice - 1) * 75 * 100).toFixed(2)
   );
