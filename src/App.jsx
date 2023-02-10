8de6de7692cd995c371fda1c0715d04acb0ac363
jhslive <111948596+jhslive@users.noreply.github.com>
2023-02-10 09:05:47 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,8 @@ var month = today.getUTCMonth() + 1; //months from 1-12
 var day = today.getUTCDate();
 var year = today.getUTCFullYear();
 
-console.log(`${year}-${month >= 10 ? month : '0' + month}-${date >= 10 ? date : '0' + date}`);
  
-today = year + "-" + month + "-" +  day;
+today = console.log(`${year}-${month >= 10 ? month : '0' + month}-${date >= 10 ? date : '0' + date}`);
 
 function App() {
   const [coin, setCoin] = useState("카나리아바이오");
