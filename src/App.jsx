0ce620cc5cbad51f5beba31fae6db08c1b0255be
jhslive <111948596+jhslive@users.noreply.github.com>
2023-02-10 09:10:25 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ var day = date.getUTCDate();
 var year = date.getUTCFullYear();
 
  
-today = console.log(`${year}-${month >= 10 ? month : '0' + month}-${date >= 10 ? today : '0' + date}`);
 
 function App() {
   const [coin, setCoin] = useState("카나리아바이오");
@@ -21,7 +20,7 @@ function App() {
   const [maxPrice, setmaxPrice] = useState("100000");
   const [isLong, setIsLong] = useState(true); //eslint-disable-line no-unused-vars
   const [leverage, setLeverage] = useState(1); //eslint-disable-line no-unused-vars
-  const [date, setdate] = useState(today);
+  const [date, setdate] = useState(console.log(`${year}-${month >= 10 ? month : '0' + month}-${date >= 10 ? today : '0' + date}`));
 
   const [result, setResult] = useState(
     ((bidPrice / askPrice - 1) * 75 * 100).toFixed(2)
